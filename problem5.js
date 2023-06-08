d2c7d4c4ca5ed1cd4e1ceb16b32a0e7dc493e323
divinestylus <divinestylus@gmail.com>
2023-06-08 14:54:47 +0000

    Problem 5

---
+++ problem5.js
@@ -0,0 +1,18 @@
+let num1 = 15;
+let num2 = 60;
+
+let i = 1;
+let divisor;
+function findDivisor(){
+    if(i <= Math.min(num1, num2)){
+        if(num1%i === 0 && num2%i === 0){
+            divisor = i;
+        }
+        i += 1;
+        findDivisor();
+    } 
+    else {
+    console.log(`${divisor} is the Greatest Common Denominator`);
+    }     
+}
+findDivisor();
